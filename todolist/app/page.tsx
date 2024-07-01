7c49cba06eb3150ddeac306c816de00660cc1ec8
nhinhiii <chaochochochao@gmail.com>
2024-07-01 20:42:18 +0700

    Check status, add, delete, move up/ down

---
+++ todolist/app/page.tsx
@@ -0,0 +1,13 @@
+import { Text } from "@chakra-ui/react";
+import TodoList from "@/components/TodoList.jsx"
+
+export default function Home() {
+  return (
+    <>
+      <Text> Test </Text>
+      <TodoList/>
+    </>
+    
+    
+  )
+}
